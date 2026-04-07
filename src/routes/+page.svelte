496b3f351f00cee748124350e7039e4bd2dab6fe
bmehder <bmehder@gmail.com>
2026-04-07 02:39:55 +0100

    hero stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,16 @@
 
 <PageTitle title="Home"></PageTitle>
 
-<section>
+<section class="hero-section">
 	<div class="outer">
 		<div class="inner">
-			<img src="/haarlem.jpg" width="1200" height="600" alt="Rocky mountain landscape with waterfalls and low clouds" />
+			<img
+				src="/haarlem.jpg"
+				width="1200"
+				height="600"
+				alt="Rocky mountain landscape with waterfalls and low clouds"
+			/>
+			<div>Welkom in Haarlem!</div>
 		</div>
 	</div>
 </section>
@@ -27,3 +33,33 @@
 		</div>
 	</div>
 </section>
+
+<style>
+	.hero-section {
+		.inner {
+			display: grid;
+			align-items: center;
+			
+			img {
+				grid-column: 1 / 2;
+				grid-row: 1 / 2;
+			}
+			
+			div {
+				grid-column: 1 / 2;
+				grid-row: 1 / 2;
+				
+				padding: 1rem;
+				background-color: rgba(0, 0, 0, 0.6);
+				
+				text-align: center;
+				text-transform: uppercase;
+				font-size: clamp(1.5rem, 3vw, 3rem);
+				font-weight: bold;
+				letter-spacing: 3px;
+				
+				border-radius: 0.25rem;
+			}
+		}
+	}
+</style>
